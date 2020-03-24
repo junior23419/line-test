4550f1eb8fa6577afb8261553292632265f8dde3
junior23419 <junior_travian@hotmail.co.th>
2020-03-24 15:02:54 +0700

    test1

--- app.js
+++ app.js
@@ -1,5 +1,42 @@
+// Reply with two static messages
+
 const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
 const app = express()
 const port = process.env.PORT || 4000
-app.post('/webhook', (req, res) => res.sendStatus(200))
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.post('/webhook', (req, res) => {
+    let reply_token = req.body.events[0].replyToken
+    reply(reply_token)
+    res.sendStatus(200)
+})
 app.listen(port)
+function reply(reply_token) {
+    let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer {ZqmVt0vzontNiw/RfJzkXVAc64LZnznK14CiM8HzK5q1Divn67alwvpt0ZPaxwa+XgbB0VPPS3NDumzQbhwUBGq5+lDtzCvCMAhrDX0YGxGo+h5ECvQXiU5OijcJWiLSrBnnbUmmY/c6Embl5O2fFwdB04t89/1O/w1cDnyilFU=}'
+    }
+    let body = JSON.stringify({
+        replyToken: reply_token,
+        messages: [{
+            type: 'text',
+            text: 'Hello'
+        },
+        {
+            type: 'text',
+            text: 'How are you?'
+        }]
+    })
+    request.post({
+        url: 'https://api.line.me/v2/bot/message/reply',
+        headers: headers,
+        body: body
+    }, (err, res, body) => {
+        console.log('status = ' + res.statusCode);
+    });
+}
